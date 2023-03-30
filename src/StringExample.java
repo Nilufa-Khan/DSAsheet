9de24a0710448baab8272349df64fcda2127d181
Nilufa Khan <khannilufa5@gmail.com>
2023-03-30 23:56:46 +0530

    String example

---
+++ src/StringExample.java
@@ -0,0 +1,19 @@
+public class StringExample {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(str);
+
+        String str2 = "Hello";
+        str = "Morning";
+        System.out.println(str);
+        System.out.println(str2);
+        str = "Hello";
+        System.out.println(str);
+        int a = 12;
+        int b = 12;
+        System.out.println(a);
+        String str3 = new String("Hello");
+        System.out.println(str3);
+
+    }
+}
